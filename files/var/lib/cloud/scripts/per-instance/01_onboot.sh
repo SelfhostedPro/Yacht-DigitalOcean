90a79202a58319d0521af085f97a40192e008deb
SelfhostedPro <info@selfhosted.pro>
2020-10-31 13:04:32 -0700

    fixed MOTD; still can't get admin pass to work correctly.

--- files/var/lib/cloud/scripts/per-instance/01_onboot.sh
+++ files/var/lib/cloud/scripts/per-instance/01_onboot.sh
@@ -7,19 +7,19 @@ export LC_CTYPE=en_US.UTF-8
 
 # Generate Yacht admin pass
 
-yacht_admin_pass=$(openssl rand -hex 24)
+# yacht_admin_pass=$(openssl rand -hex 24)
 
 # Save the password
-cat > /root/.yacht_password <<EOM
-yacht_admin_pass="${yacht_admin_pass}"
-EOM
+# cat > /root/.yacht_password <<EOM
+# yacht_admin_pass="${yacht_admin_pass}"
+# EOM
 
 
 # Launch container with the password
 docker run -d \
+    --name=yacht \
     -p 8000:8000/tcp \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /opt/Yacht:/config \
-    -e "ADMIN_PASS=${yacht_admin_pass}" \
     --restart=always \
     selfhostedpro/yacht:do
